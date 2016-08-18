03ca0a24d0c64a77a1baa4a315bf59d2c0e37f06
Dmitrii Semeniuc <dmitriisemeniuc@gmail.com>
2016-08-18 16:20:52 +0300

    [RMP-1] Create MainActivity class and layout. Fixed

--- app/src/main/java/com/semeniuc/dmitrii/remindmeproject/MainActivity.java
+++ app/src/main/java/com/semeniuc/dmitrii/remindmeproject/MainActivity.java
@@ -2,15 +2,34 @@ package com.semeniuc.dmitrii.remindmeproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Dmitrii on 8/18/2016.
  */
 public class MainActivity extends Activity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
