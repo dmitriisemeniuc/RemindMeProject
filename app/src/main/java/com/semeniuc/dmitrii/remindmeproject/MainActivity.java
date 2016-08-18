cb2c349b4bce4f10ae19cc564b10d62e02cdeb24
Dmitrii Semeniuc <dmitriisemeniuc@gmail.com>
2016-08-18 14:00:19 +0300

    [RMP-1] Create MainActivity class and layout

---
+++ app/src/main/java/com/semeniuc/dmitrii/remindmeproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.semeniuc.dmitrii.remindmeproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dmitrii on 8/18/2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
