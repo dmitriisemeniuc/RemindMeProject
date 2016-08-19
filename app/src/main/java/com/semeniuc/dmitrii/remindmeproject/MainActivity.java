36ac1acf35ad250e29cef237d0538efbd222cadd
Dmitrii Semeniuc <dmitriisemeniuc@gmail.com>
2016-08-19 11:02:12 +0300

    [RMP-6] Add Notification button to NavigationView

--- app/src/main/java/com/semeniuc/dmitrii/remindmeproject/MainActivity.java
+++ app/src/main/java/com/semeniuc/dmitrii/remindmeproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.semeniuc.dmitrii.remindmeproject;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -10,15 +11,18 @@ import android.view.MenuItem;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -32,5 +36,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
