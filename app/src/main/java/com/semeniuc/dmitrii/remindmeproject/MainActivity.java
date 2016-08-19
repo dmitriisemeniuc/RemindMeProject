5b3c85cd101dd00541aeefcfdf51316d26c02ab1
Dmitrii Semeniuc <dmitriisemeniuc@gmail.com>
2016-08-19 10:29:27 +0300

    RMP-4 Create Mock-up for Navigation Drawer

--- app/src/main/java/com/semeniuc/dmitrii/remindmeproject/MainActivity.java
+++ app/src/main/java/com/semeniuc/dmitrii/remindmeproject/MainActivity.java
@@ -1,19 +1,20 @@
 package com.semeniuc.dmitrii.remindmeproject;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 /**
  * Created by Dmitrii on 8/18/2016.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
